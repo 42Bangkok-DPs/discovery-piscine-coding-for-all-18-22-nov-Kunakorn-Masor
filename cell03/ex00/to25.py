6aebf3b3164b336056730f8ef66749cefa5c634e
Kunakorn-Masor <67030037@kmitl.ac.th>
2024-11-21 01:17:45 +0000

    to25.py

--- cell03/ex00/to25.py
+++ cell03/ex00/to25.py
@@ -1,5 +1,4 @@
-try:
-    num = int(input("Enter a number less than 25: "))
+num = int(input("Enter a number less than 25: "))
     
     if num > 25:
         print("Error")
@@ -8,6 +7,4 @@ try:
         while num <= 25:
             print(f"Inside the loop, my variable is {num}")
             num += 1  
-except ValueError:
-    
-    print("Please enter a valid number.")
+
